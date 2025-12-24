692b9ad6590a4eee8f2b75ce5feb9934f652e29f
HP <HP@DESKTOP-HINS88F>
2025-12-24 14:02:48 -0600

    Cambios

---
+++ CrossBoxApp/Models/PerfilDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossBoxApp.Models
+{
+    public class PerfilDto
+    {
+        public Guid PerfilID { get; set; }
+        public Guid UsuarioID { get; set; }
+        public string FotoPerfil { get; set; } // Guardará la URL o nombre del archivo
+        public string Descripcion { get; set; }
+        public string Whatsapp { get; set; }
+        public int TotalLikes { get; set; }
+    }
+}
