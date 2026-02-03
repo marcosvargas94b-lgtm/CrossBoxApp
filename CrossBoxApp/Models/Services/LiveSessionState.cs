8b25f47ac554d9cbf75ff15906c493afa54538d9
MVargas <marcosvargas421@gmail.com>
2026-02-03 16:23:54 -0600

    Cambios

--- CrossBoxApp/Models/Services/LiveSessionState.cs
+++ CrossBoxApp/Models/Services/LiveSessionState.cs
@@ -10,5 +10,16 @@ namespace CrossBoxApp.Models.Services
     {
         public List<CheckInDto> AtletasEnClase { get; set; } = new List<CheckInDto>();
         public Guid BoxId { get; set; }
+        public List<ResultadoCapturadoViewModel> ResultadosJuez { get; set; } = new();
+        public bool FueModoJuez { get; set; } = false;
+    }
+
+    public class ResultadoCapturadoViewModel
+    {
+        public Guid IdIdentificador { get; set; } = Guid.NewGuid(); // ID temporal para la UI
+        public List<CheckInDto> Atletas { get; set; } = new(); // Puede ser 1 o 2
+        public string NombreEquipo { get; set; } // Opcional
+        public string TiempoFinal { get; set; } // "12:45"
+        public bool Finalizado { get; set; } = false; // Para ponerlo en verde/gris
     }
 }
