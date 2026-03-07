d49093069821f0e0fbe8a0720d2f2731852b6a8e
HP <HP@DESKTOP-HINS88F>
2026-03-07 12:56:30 -0600

    Cambios, viene lo dificil

--- CrossBoxApp/Models/RecuperacionModels.cs
+++ CrossBoxApp/Models/RecuperacionModels.cs
@@ -8,6 +8,13 @@ namespace CrossBoxApp.Models
         public string Mensaje { get; set; }
         public List<MusculoRecuperacionDto> Musculos { get; set; } = new List<MusculoRecuperacionDto>();
         public List<VolumenItem> VolumenSemanal { get; set; } = new List<VolumenItem>();
+        public bool TieneDatosFisiologicos { get; set; }
+        public Guid? RutinaHoyParaRPE { get; set; }
+        public bool RequiereScaling { get; set; }
+        public int? Edad { get; set; }
+        public decimal? Peso { get; set; }
+        public decimal? Estatura { get; set; }
+        public string Sexo { get; set; }
     }
 
     public class MusculoRecuperacionDto
@@ -15,6 +22,7 @@ namespace CrossBoxApp.Models
         public string Nombre { get; set; }
         public int Porcentaje { get; set; }
         public int HorasParaRecuperacion { get; set; }
+        
     }
 
     public class VolumenItem
